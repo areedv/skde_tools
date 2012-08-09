cf017e4387f1a6ce1b4af36bbd24baec2b9d521e
aed <aed@437c27f3-ed2e-4eaa-90ef-efc7fbd1295b>
2012-08-09 08:52:02 +0000

    first import
    git-svn-id: svn://172.19.15.126/jasper/skde_tools/trunk@6710 437c27f3-ed2e-4eaa-90ef-efc7fbd1295b

---
+++ src/no/skde/tools/report/ProcessSweave.java
@@ -0,0 +1,166 @@
+/**
+ * no.helsenord.tools.report
+ * ProcessSweave.java Aug 8 2012 Are Edvardsen
+ * 
+ * 
+ *  Copyleft 2011, 2012 SKDE
+ */
+
+package no.skde.tools.report;
+
+import java.io.*;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.fill.*;
+import org.apache.log4j.Logger;
+//import no.skde.tools.report.*;
+import no.helseregister.tools.security.*;
+import org.rosuda.REngine.*;
+import org.rosuda.REngine.Rserve.*;
+
+public class ProcessSweave extends JRDefaultScriptlet {
+	protected RConnection rconn;
+	
+	private String jasperReportFeedback;
+	
+	private HRegUser hregUser = null;
+	
+	private String attachmentPathFileName;
+	
+	static Logger log = Logger.getLogger("report");
+	
+	
+	// getters and setters
+	public void setJasperReportFeedback(String jasperReportFeedback) {
+		this.jasperReportFeedback = jasperReportFeedback;
+	}
+
+	public String getJasperReportFeedback() {
+		return jasperReportFeedback;
+	}
+	
+	public void setAttachmentPathFileName(String attachmentPathFileName) {
+		this.attachmentPathFileName = attachmentPathFileName;
+	}
+	
+	public String getAttachmentPathFileName() {
+		return attachmentPathFileName;
+	}
+	
+	
+	// override empty method of JRDefaultScriptlet
+	public void afterReportInit() throws JRScriptletException {
+		// get hreg_key from report report params
+		if (parametersMap.containsKey("hreg_key")) {
+			String hreg_key = (String) ((JRFillParameter) parametersMap
+					.get("hreg_key")).getValue();
+			if (hreg_key != null) {
+				hreg_key = hreg_key.replaceAll(" ", "+");
+				loginUser(hreg_key);
+			}
+			log.debug("parametersMap contains Key ='hreg_key' : "
+					+ hreg_key);
+		} else {
+			log.warn("parametersMap do not contain Key ='hreg_key'");
+		}
+		
+		generateReport();
+
+		super.afterReportInit();
+	}
+	
+	
+	// report actions
+	private void generateReport() {
+		try {
+			rconn = new RConnection();
+			
+			log.info("Start to generate report" + ProcessSweave.class.getName());
+			log.info("Report requested by " + hregUser.getQregUserFirstName()
+					+ " " + hregUser.getQregUserLastName());
+			
+			// get report parameters
+			String reportFileName = (String) ((JRFillParameter) parametersMap.get("reportFileName")).getValue();
+			if (reportFileName == "") {
+				log.warn("reportFileName is empty. Eventually, processing an undfined report will fail");
+			}
+			
+			Integer doSendEmail = (Integer) ((JRFillParameter) parametersMap.get("doSendEmail")).getValue();
+			boolean dryRun = false;
+			if (doSendEmail == 0) {
+				dryRun = true;
+			}
+			
+			String emailSubject = (String) ((JRFillParameter) parametersMap.get("emailSubject")).getValue();
+			if (emailSubject == "") {
+				log.warn("No email subject provided, might confuse the recipient...");
+			}
+			
+			// make Sweave and LaTeX 
+			log.debug("Doing Sweave and LaTeX...");
+			REXP rWorkdir = rconn.eval("getwd()");
+			String workdir = rWorkdir.asString();
+			log.debug("The Rserve session's current working directry is: " + workdir);
+			rconn.assign("workfile", "../" + reportFileName);
+			rconn.assign("reportTmpFileName", reportFileName);
+			REXP workfilename = rconn.eval("paste(workfile, '.Rnw', sep='')");
+			log.debug("Rserve current workfile is: " + workfilename.asString());
+			rconn.voidEval("Sweave(paste(workfile, '.Rnw', sep=''))");
+			rconn.voidEval("tools::texi2dvi(paste(reportTmpFileName, '.tex', sep=''), pdf=T, clean=T)");
+			
+			// make a temporary file name for attachment
+			File tmpFile = new File(workdir);
+			String attachmentFile = (File.createTempFile(reportFileName, ".pdf", tmpFile)).getName();
+			log.debug("Attachment file name: " + attachmentFile);
+			setAttachmentPathFileName(workdir + "/" + attachmentFile);
+			log.debug("Attachment file name and path: " + getAttachmentPathFileName());
+			
+			// copy report to temp file
+			log.debug("Rserve assignment: reportFileNamePath<-" + getAttachmentPathFileName());
+			rconn.assign("reportFileNamePath", getAttachmentPathFileName());
+			log.debug("Rserve assignment: reportTmpFileName <- " + reportFileName);
+			
+			log.debug("Rserve file copy from: " + reportFileName + ".pdf to " + getAttachmentPathFileName());
+			
+			rconn.voidEval("file.copy(paste(reportTmpFileName, '.pdf', sep=''), reportFileNamePath, overwrite=T)");
+			log.debug("Report is ready to be shipped by email");
+			
+			sendEmail(emailSubject, dryRun);
+			
+			rconn.close();
+			rconn = null;
+		} catch (RserveException rse) {
+			log.error("Rserv exception " + rse.getMessage());
+			rconn.close();
+			rconn = null;
+		} catch (Exception e) {
+			log.error("Something went wrong, but it is not Rserv: " + e.getMessage());
+			rconn.close();
+			rconn = null;
+		}
+		
+	}
+	
+	private void sendEmail(String emailSubject, boolean dryRun) {
+		log.debug("Report is to be shipped by email");
+		try {
+			SendReportByEmail sender = new SendReportByEmail();
+			String userEmailAddress = hregUser.getHregUserEmail();
+			sender.sendEmail(userEmailAddress, emailSubject, getAttachmentPathFileName(), dryRun);
+			log.info("Report sent to " + userEmailAddress);
+			setJasperReportFeedback("Epost sendt til " + userEmailAddress);
+		} catch (Exception e) {
+			setJasperReportFeedback("Feil: epost ble ikke sendt!");
+			log.error("Could not send email: " + e.getMessage());
+		}
+	}
+	
+	private void loginUser(String hreg_key) throws JRScriptletException {
+		log.debug("loginUser: ");
+		try {
+			hregUser = new HRegUser(hreg_key, "certFile.cert", true);
+			log.debug("Valid user, moving on...");
+		} catch (Exception e) {
+			log.warn("Couldn't create HRegUser from parameter. " + e);
+		}
+	}
+}
---
+++ src/no/skde/tools/report/SendReportByEmail.java
@@ -0,0 +1,142 @@
+/**
+ * no.helsenord.tools.report
+ * SendReportByEmail.java Jul 6 2011 Are Edvardsen
+ * 
+ * This file is (so far the only) part of the skde.no libraries
+ * 
+ *  Copyleft 2011 SKDE
+ */
+
+package no.skde.tools.report;
+
+import java.io.*;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+import org.apache.log4j.Logger;
+
+public class SendReportByEmail {
+
+//	protected static Logger log = Logger.getLogger(SendReportByEmail.class);
+	protected static Logger log = Logger.getLogger("report");
+	
+	private static final String  MAIL_SERVER_CONFIG_PATH_FILE = 
+		"/opt/jasper/mail/conf/MailServerReport.conf";
+	
+	public void sendEmail (String toAddress, String subject,
+			String attachmentFile, boolean dryRun) {
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(fMailServerConfig, null);
+
+	      try {
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO,
+	                                  new InternetAddress(toAddress));
+
+	         // Set Subject: header field
+	         message.setSubject(subject);
+	         
+	         // create the message part
+	         BodyPart messageBodyPart = new MimeBodyPart();
+
+	         // set the actual message, a general from file
+	         String emailMessage = fileToString("/opt/jasper/mail/message/general.txt");
+	         messageBodyPart.setText(emailMessage);
+	         
+	         // create a multipart message
+	         Multipart multipart = new MimeMultipart();
+	         
+	         // set text message part
+	         multipart.addBodyPart(messageBodyPart);
+	         
+	         // second part is attachment
+	         messageBodyPart = new MimeBodyPart();
+//	         String fileName = "attachment.txt";
+	         String[] attachment = attachmentFile.split("/");
+	         String attachmentFileName = attachment[attachment.length-1];
+	         DataSource source = new FileDataSource(attachmentFile);
+	         messageBodyPart.setDataHandler(new DataHandler(source));
+	         messageBodyPart.setFileName(attachmentFileName);
+	         multipart.addBodyPart(messageBodyPart);
+
+	         // complete message parts
+	         message.setContent(multipart);
+	         
+	         // Send message
+	         if (dryRun) {
+	        	 log.info("Everything ok, but Email not actually sent since dryRun is set true.");
+	         }
+	         else {
+	        	 Transport.send(message);
+	        	 log.info("Email was sent successfully.");
+	         }
+	      }
+	      catch (MessagingException e) {
+	         log.error("Could not send email: " + e);
+	      }
+	   }
+
+	
+	   // read file content
+	   public static String fileToString(String fileName) {
+		   File file = new File(fileName);
+		   StringBuilder contents = new StringBuilder();
+		   BufferedReader input = null;
+		   try {
+			   input = new BufferedReader(new FileReader(file));
+			   String line = null;
+			   while ((line = input.readLine()) != null)
+				   contents.append(line);
+		   }
+		   catch (Exception ex) {
+			   ex.printStackTrace();
+		   }
+		   finally {
+			   try {
+				   if(input != null) input.close();
+			   }
+			   catch (Exception ex) {
+				   ex.printStackTrace();
+			   }
+		   }
+		   return contents.toString();
+	   }
+	   
+	   /**
+	    * Allows config to be refreshed at runtime
+	    */
+	   public static void refreshConfig() {
+		   fMailServerConfig.clear();
+		   fetchConfig();
+	   }
+	   
+	   private static Properties fMailServerConfig = new Properties();
+	   
+	   static {
+		   fetchConfig();
+	   }
+	   
+	   private static void fetchConfig() {
+		   InputStream input = null;
+		   try {
+			   input = new FileInputStream(MAIL_SERVER_CONFIG_PATH_FILE);
+			   fMailServerConfig.load( input );
+		   }
+		   catch (IOException e) {
+			   log.error("Cannot open/load conf file: " + e);
+		   }
+		   finally {
+			   try {
+				   if ( input != null ) input.close();
+			   }
+			   catch ( IOException e) {
+				   log.error("Cannot close conf file: " + e);
+			   }
+		   }
+	   }
+	
+}
