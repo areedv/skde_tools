19da66d49ffc40a0335689a36cc8ab146e329454
aed <aed@437c27f3-ed2e-4eaa-90ef-efc7fbd1295b>
2015-08-14 11:21:50 +0000

    new class for use with R packages
    git-svn-id: svn://172.19.15.126/jasper/skde_tools/trunk@21152 437c27f3-ed2e-4eaa-90ef-efc7fbd1295b

---
+++ src/no/skde/report/norscir/NorScirCommonScriptletRPackage.java
@@ -0,0 +1,306 @@
+/**
+ * no.skde.report.norscir
+ * NorScirCommonScriptletRPackage.java Aug 14 2015 Are Edvardsen
+ * 
+ * 
+ *  Copyleft 2015, SKDE
+ */
+
+package no.skde.report.norscir;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.fill.*;
+
+import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.rosuda.REngine.*;
+import org.rosuda.REngine.Rserve.*;
+
+public class NorScirCommonScriptletRPackage extends JRDefaultScriptlet {
+
+	protected RConnection rconn;
+	private String fileName;
+	
+	static Logger log = Logger.getLogger("report");
+	
+	// getters and setters
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	
+	// override empty method of JRDefaultScriptlet
+	public void afterReportInit() throws JRScriptletException {
+		log.debug("scriptlet started, 'afterReportInit()'...");
+		generateReport();
+
+		super.afterReportInit();
+	}
+	
+	
+	// report actions
+	@SuppressWarnings("unchecked")
+	private void generateReport() {
+		try {
+			log.info("Start generating R report using " + NorScirCommonScriptletRPackage.class.getName());
+			
+			// Create the connection
+			log.debug("Getting connection to R instance...");
+			rconn = new RConnection();
+			log.debug("R connection provided: " + rconn.toString());
+
+
+			// Get parameters
+			// these must always be provided when this scriptlet class is used
+			String loggedInUserFullName = "";
+			String loggedInUserAVD_RESH = "";
+			String reportName = "";
+			String rFunctionCallString = "";
+			try {
+				loggedInUserFullName = (String) ((JRFillParameter) parametersMap.get("LoggedInUserFullName")).getValue();
+				loggedInUserAVD_RESH = (String) ((JRFillParameter) parametersMap.get("LoggedInUserAVD_RESH")).getValue();
+				reportName = (String) ((JRFillParameter) parametersMap.get("reportName")).getValue();
+				rFunctionCallString = (String) ((JRFillParameter) parametersMap.get("rFunctionCallString")).getValue();
+				log.info("Report to be run: " + reportName);
+				log.info("Report requested by JRS user " + loggedInUserFullName + ", AVD_RESH " + loggedInUserAVD_RESH);
+				log.debug("R function call string: " + rFunctionCallString);
+			} catch (Exception e) {
+				log.error("Mandatory parameters in the report definition calling this scriptlet were not defined: " + e.getMessage());
+			}
+			
+			// the rest of parameters are optional, but must match whatever needed by R
+			String varName;
+			try {
+				log.debug("Getting parameter values");
+				varName = (String) ((JRFillParameter) parametersMap.get("varName")).getValue();
+				if (varName == null) {
+					varName = "nada";
+				}
+				rconn.voidEval("valgtVar=" + "'" + varName + "'");
+			} catch (Exception e) {
+				log.debug("Parameter varName is not defined: " + e.getMessage());
+			}
+			
+			String statMeasureMethod;
+			try {
+				statMeasureMethod = (String) ((JRFillParameter) parametersMap.get("statMeasureMethod")).getValue();
+				if (statMeasureMethod == null) {
+					statMeasureMethod = "Gjsn";
+				}
+				rconn.voidEval("valgtMaal=" + "'" + statMeasureMethod.toString() + "'");
+			} catch (Exception e) {
+				log.debug("Parameter statMeasureMethod is not defined: " + e.getMessage());
+			}			
+			
+			Integer minAge;
+			try {
+				minAge = (Integer) ((JRFillParameter) parametersMap.get("minAge")).getValue();
+				if (minAge == null) {
+					minAge = 0;
+				}
+				rconn.voidEval("minald=" + minAge.toString());
+			} catch (Exception e) {
+				log.debug("Parameter minAge is not defined: " + e.getMessage());
+			}
+
+			Integer maxAge;
+			try {
+				maxAge = (Integer) ((JRFillParameter) parametersMap.get("maxAge")).getValue();
+				if (maxAge == null) {
+					maxAge = 130;
+				}
+				rconn.voidEval("maxald=" + maxAge.toString());
+			} catch (Exception e) {
+				log.debug("Parameter maxAge is not defined: " + e.getMessage());
+			}
+
+			
+			// convert dates to something that can be understood by R
+			SimpleDateFormat rFormat = new SimpleDateFormat("yyyy-MM-dd");
+			
+			Date startDate;
+			try {
+				startDate = (Date) ((JRFillParameter) parametersMap.get("startDate")).getValue();
+				if (startDate == null) {
+					startDate = new SimpleDateFormat("yyyy-MM-dd").parse("2010-01-01");
+				}
+				StringBuilder startDateString = new StringBuilder(rFormat.format(startDate));
+				rconn.voidEval("datoFra=" + "'" + startDateString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter startDate is not defined: " + e.getMessage());
+			}
+
+			Date endDate;
+			try {
+				endDate = (Date) ((JRFillParameter) parametersMap.get("endDate")).getValue();
+				if (endDate == null) {
+					endDate = new Date();
+				}
+				StringBuilder endDateString = new StringBuilder(rFormat.format(endDate));
+				rconn.voidEval("datoTil=" + "'" + endDateString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter endDate is not defined: " + e.getMessage());
+			}
+			
+			String hospitalsLevel;
+			try {
+				hospitalsLevel = (String) ((JRFillParameter) parametersMap.get("hospitalsLevel")).getValue();
+				if (hospitalsLevel == null) {
+					hospitalsLevel = "region";
+				}
+				rconn.voidEval("ShType=" + "'" + hospitalsLevel.toString() + "'");
+			} catch (Exception e) {
+				log.debug("Parameter hospitalsLevel is not defined: " + e.getMessage());
+			}			
+			
+			Integer inFromSituation;
+			try {
+				inFromSituation = (Integer) ((JRFillParameter) parametersMap.get("inFromSituation")).getValue();
+				if (inFromSituation == null) {
+					inFromSituation = 99;
+				}
+
+				rconn.voidEval("InnMaate=" + inFromSituation.toString());
+			} catch (Exception e) {
+				log.debug("Parameter inFromSituation is not defined: " + e.getMessage());
+			}
+			
+			
+			String isTrauma;
+			try {
+				isTrauma = (String) ((JRFillParameter) parametersMap.get("isTrauma")).getValue();
+				if (isTrauma == null) {
+					isTrauma = "na";
+				}
+
+				rconn.voidEval("traume=" + "'" + isTrauma.toString() + "'");
+			} catch (Exception e) {
+				log.debug("Parameter isTrauma is not defined: " + e.getMessage());
+			}
+			
+			Integer erMann;
+			try {
+				erMann = (Integer) ((JRFillParameter) parametersMap.get("erMann")).getValue();
+				if (erMann == null) {
+					erMann = 99;
+				}
+				rconn.voidEval("erMann=" + erMann.toString());
+			} catch (Exception e) {
+				log.debug("Parameter erMann is not defined: " + e.getMessage());
+			}
+			
+			Integer myDept;
+			try {
+				myDept = (Integer) ((JRFillParameter) parametersMap.get("myDept")).getValue();
+				if (myDept == null) {
+					myDept = 1;
+				}
+				log.warn("Parameter 'myDept' is deprecated and may be removed in future versions. Replaced by 'orgUnitSelection'");
+				rconn.voidEval("egenavd=" + myDept.toString());
+			} catch (Exception e) {
+				log.debug("Parameter myDept is not defined: " + e.getMessage());
+			}
+			
+			Integer orgUnitSelection;
+			try {
+				orgUnitSelection =  (Integer) ((JRFillParameter) parametersMap.get("orgUnitSelection")).getValue();
+				if (orgUnitSelection == null) {
+					orgUnitSelection = 1;
+				}
+				rconn.voidEval("enhetsUtvalg=" + orgUnitSelection.toString());
+			} catch (Exception e) {
+				log.debug("Parameter orgUnitSelection is not defined: " + e.getMessage());
+			}
+			
+			// AIS; multi select list of values
+			List<String> aisList = new ArrayList<String>();
+			String ais;
+			try {
+				aisList = (ArrayList<String>) ((JRFillParameter) parametersMap.get("ais")).getValue();
+				ais = "c(";
+				if (aisList.contains("all")) {
+					ais = ais + "'')";
+				} else {
+					Iterator<String> iterator = aisList.iterator();
+					while (iterator.hasNext()) {
+						ais = ais + "'" + iterator.next() + "',";
+					}
+					ais = ais.substring(0, ais.length()-1);
+					ais = ais + ")";
+				}
+				log.debug("R concat for AIS vector is " + ais);
+				rconn.voidEval("AIS=" + ais);
+			} catch (Exception e) {
+				log.debug("Parameter ais is not defined: " + e.getMessage());
+			}
+			
+			
+			// Now, removed loading of data through this scriptlet
+			log.info("RegData no is no longer provided by the NorScir scriptlet");
+			
+			
+			log.debug("Creating dummy R dataframe to ensure compatibility with existing R scripts");
+
+			RList l = new RList();
+			
+			// anything goes...
+			l.put("Rapportnavn", new REXPString(reportName));
+
+			REXP df = REXP.createDataFrame(l);
+			log.debug("Assigning data frame to R instance");
+			rconn.assign("RegData", df);
+			
+			
+			// Set up the tmp directory, file names and reportUserInfo
+			String tmpdir = "";
+			String p_filename = "";
+			log.debug("Setting report image filepath and name");
+			tmpdir = "/opt/jasper/img/";
+			File dirFile = new File(tmpdir);
+			String fileBaseName = "norScir_" + reportName + "-";
+			String file = (File.createTempFile(fileBaseName, ".png", dirFile)).getName();
+			p_filename = tmpdir + file;
+			log.debug("In R instance: image to be stored as: " + p_filename);
+			setFileName(p_filename);
+	
+			log.debug("In R instance: assigning Filename: " + p_filename);
+			rconn.assign("outfile", p_filename);
+			
+			// do I really need to make a new string for this?
+			String rcmd = rFunctionCallString;
+			
+			// Call the function to generate the report
+			log.debug("In R instance: calling function");
+			rconn.voidEval(rcmd);
+			
+			// Close RServ connection, ensure garbage collection removes pointer too!
+			log.debug("Closing connection to R instance and removing pointer");
+			rconn.close();
+			rconn = null;
+			log.info("Finished report");
+
+		} catch (RserveException rse) {
+			log.error("Rserve exception: " + rse.getMessage());
+			log.error("Gory details in Rserve.log");
+			rconn.close();
+			rconn = null;
+		} catch (Exception e) {
+			log.error("Something went wrong, but it's not the Rserve: " + e.getMessage());
+			e.printStackTrace();
+			rconn.close();
+			rconn = null;
+		}
+	}
+}
