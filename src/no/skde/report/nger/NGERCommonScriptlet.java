233a70d5374bf4fe26c4f589e0b0183fa630581a
aed <aed@437c27f3-ed2e-4eaa-90ef-efc7fbd1295b>
2015-09-03 12:37:36 +0000

    ### temporary import awaiting possible data set
    git-svn-id: svn://172.19.15.126/jasper/skde_tools/trunk@21172 437c27f3-ed2e-4eaa-90ef-efc7fbd1295b

--- src/no/skde/report/nger/NGERCommonScriptlet.java
+++ src/no/skde/report/nger/NGERCommonScriptlet.java
@@ -15,7 +15,7 @@ import java.util.Date;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.fill.*;
 import org.apache.log4j.Logger;
-import org.rosuda.REngine.*;
+//import org.rosuda.REngine.*;
 import org.rosuda.REngine.Rserve.*;
 
 public class NGERCommonScriptlet extends JRDefaultScriptlet {
