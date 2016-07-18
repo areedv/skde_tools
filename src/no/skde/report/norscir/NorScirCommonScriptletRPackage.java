5e0b4ceb97ab76d0ca19c144bdf2d8469ef37c62
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-07-18 12:38:54 +0200

    fix missing reshID in R session

--- src/no/skde/report/norscir/NorScirCommonScriptletRPackage.java
+++ src/no/skde/report/norscir/NorScirCommonScriptletRPackage.java
@@ -76,6 +76,7 @@ public class NorScirCommonScriptletRPackage extends JRDefaultScriptlet {
 				log.info("Report to be run: " + reportName);
 				log.info("Report requested by JRS user " + loggedInUserFullName + ", AVD_RESH " + loggedInUserAVD_RESH);
 				log.debug("R function call string: " + rFunctionCallString);
+				rconn.voidEval("reshID=" + loggedInUserAVD_RESH);
 			} catch (Exception e) {
 				log.error("Mandatory parameters in the report definition calling this scriptlet were not defined: " + e.getMessage());
 			}
