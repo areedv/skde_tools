39754cb6bebc8cefd64d3859013f85a089a52e0f
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-11-15 15:05:02 +0100

    5 fold each generic uc

--- src/no/skde/tools/report/CommonReport.java
+++ src/no/skde/tools/report/CommonReport.java
@@ -194,15 +194,15 @@ public class CommonReport extends JRDefaultScriptlet {
 			
 			
 			// START generic, common report user controls
-			List<String> multiValgList = new ArrayList<String>();
+			List<String> multiValgList_1 = new ArrayList<String>();
 			String multiValg_1;
 			try {
-				multiValgList = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_1")).getValue();
+				multiValgList_1 = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_1")).getValue();
 				multiValg_1 = "c(";
-				if (multiValgList.isEmpty()) {
+				if (multiValgList_1.isEmpty()) {
 					multiValg_1 = multiValg_1 + "'')";
 				} else {
-					Iterator<String> iterator = multiValgList.iterator();
+					Iterator<String> iterator = multiValgList_1.iterator();
 					while (iterator.hasNext()) {
 						multiValg_1 = multiValg_1 + "'" + iterator.next() + "',";
 					}
@@ -215,40 +215,206 @@ public class CommonReport extends JRDefaultScriptlet {
 				log.debug("Parameter multiValg_1 is not defined: " + e.getMessage());
 			}
 
-			Integer enkeltValgInteger_1;
+			List<String> multiValgList_2 = new ArrayList<String>();
+			String multiValg_2;
 			try {
-				enkeltValgInteger_1 = (Integer) ((JRFillParameter) parametersMap.get("enkeltValgInteger_1")).getValue();
-				if (enkeltValgInteger_1 == null) {
-					enkeltValgInteger_1 = 99;
+				multiValgList_2 = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_2")).getValue();
+				multiValg_2 = "c(";
+				if (multiValgList_2.isEmpty()) {
+					multiValg_2 = multiValg_2 + "'')";
+				} else {
+					Iterator<String> iterator = multiValgList_2.iterator();
+					while (iterator.hasNext()) {
+						multiValg_2 = multiValg_2 + "'" + iterator.next() + "',";
+					}
+					multiValg_2 = multiValg_2.substring(0, multiValg_2.length()-1);
+					multiValg_2 = multiValg_2 + ")";
 				}
-				rconn.voidEval("enkeltValgInteger_1=" + enkeltValgInteger_1.toString());
+				log.debug("R concat for multiValg_2 vector is " + multiValg_2);
+				rconn.voidEval("multiValg_2=" + multiValg_2);
 			} catch (Exception e) {
-				log.debug("Parameter enkeltValgInteger_1 is not defined: " + e.getMessage());
+				log.debug("Parameter multiValg_2 is not defined: " + e.getMessage());
 			}
 
-			String enkeltValgStreng_1;
+			List<String> multiValgList_3 = new ArrayList<String>();
+			String multiValg_3;
+			try {
+				multiValgList_3 = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_3")).getValue();
+				multiValg_3 = "c(";
+				if (multiValgList_3.isEmpty()) {
+					multiValg_3 = multiValg_3 + "'')";
+				} else {
+					Iterator<String> iterator = multiValgList_3.iterator();
+					while (iterator.hasNext()) {
+						multiValg_3 = multiValg_3 + "'" + iterator.next() + "',";
+					}
+					multiValg_3 = multiValg_3.substring(0, multiValg_3.length()-1);
+					multiValg_3 = multiValg_3 + ")";
+				}
+				log.debug("R concat for multiValg_1 vector is " + multiValg_3);
+				rconn.voidEval("multiValg_1=" + multiValg_3);
+			} catch (Exception e) {
+				log.debug("Parameter multiValg_3 is not defined: " + e.getMessage());
+			}
+
+			List<String> multiValgList_4 = new ArrayList<String>();
+			String multiValg_4;
+			try {
+				multiValgList_4 = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_4")).getValue();
+				multiValg_4 = "c(";
+				if (multiValgList_4.isEmpty()) {
+					multiValg_4 = multiValg_4 + "'')";
+				} else {
+					Iterator<String> iterator = multiValgList_4.iterator();
+					while (iterator.hasNext()) {
+						multiValg_4 = multiValg_4 + "'" + iterator.next() + "',";
+					}
+					multiValg_4 = multiValg_4.substring(0, multiValg_4.length()-1);
+					multiValg_4 = multiValg_4 + ")";
+				}
+				log.debug("R concat for multiValg_4 vector is " + multiValg_4);
+				rconn.voidEval("multiValg_4=" + multiValg_4);
+			} catch (Exception e) {
+				log.debug("Parameter multiValg_4 is not defined: " + e.getMessage());
+			}
+
+			List<String> multiValgList_5 = new ArrayList<String>();
+			String multiValg_5;
+			try {
+				multiValgList_5 = (List<String>) ((JRFillParameter) parametersMap.get("multiValg_5")).getValue();
+				multiValg_5 = "c(";
+				if (multiValgList_5.isEmpty()) {
+					multiValg_5 = multiValg_5 + "'')";
+				} else {
+					Iterator<String> iterator = multiValgList_5.iterator();
+					while (iterator.hasNext()) {
+						multiValg_5 = multiValg_5 + "'" + iterator.next() + "',";
+					}
+					multiValg_5 = multiValg_5.substring(0, multiValg_5.length()-1);
+					multiValg_5 = multiValg_5 + ")";
+				}
+				log.debug("R concat for multiValg_5 vector is " + multiValg_5);
+				rconn.voidEval("multiValg_5=" + multiValg_5);
+			} catch (Exception e) {
+				log.debug("Parameter multiValg_5 is not defined: " + e.getMessage());
+			}
+
+			
+			Integer integer_1;
+			try {
+				integer_1 = (Integer) ((JRFillParameter) parametersMap.get("integer_1")).getValue();
+				if (integer_1 == null) {
+					integer_1 = 99;
+				}
+				rconn.voidEval("integer_1=" + integer_1.toString());
+			} catch (Exception e) {
+				log.debug("Parameter iInteger_1 is not defined: " + e.getMessage());
+			}
+			
+			Integer integer_2;
+			try {
+				integer_2 = (Integer) ((JRFillParameter) parametersMap.get("integer_2")).getValue();
+				if (integer_2 == null) {
+					integer_2 = 99;
+				}
+				rconn.voidEval("integer_2=" + integer_2.toString());
+			} catch (Exception e) {
+				log.debug("Parameter integer_2 is not defined: " + e.getMessage());
+			}
+			
+			Integer integer_3;
+			try {
+				integer_3 = (Integer) ((JRFillParameter) parametersMap.get("integer_3")).getValue();
+				if (integer_3 == null) {
+					integer_3 = 99;
+				}
+				rconn.voidEval("integer_3=" + integer_3.toString());
+			} catch (Exception e) {
+				log.debug("Parameter integer_3 is not defined: " + e.getMessage());
+			}
+			
+			Integer integer_4;
+			try {
+				integer_4 = (Integer) ((JRFillParameter) parametersMap.get("integer_4")).getValue();
+				if (integer_4 == null) {
+					integer_4 = 99;
+				}
+				rconn.voidEval("integer_4=" + integer_4.toString());
+			} catch (Exception e) {
+				log.debug("Parameter integer_4 is not defined: " + e.getMessage());
+			}
+			
+			Integer integer_5;
+			try {
+				integer_5 = (Integer) ((JRFillParameter) parametersMap.get("integer_5")).getValue();
+				if (integer_5 == null) {
+					integer_5 = 99;
+				}
+				rconn.voidEval("integer_5=" + integer_5.toString());
+			} catch (Exception e) {
+				log.debug("Parameter integer_5 is not defined: " + e.getMessage());
+			}
+			
+			String streng_1;
 			try {
 				log.debug("Getting parameter values");
-				enkeltValgStreng_1 = (String) ((JRFillParameter) parametersMap.get("enkeltValgStreng_1")).getValue();
-				if (enkeltValgStreng_1 == null) {
-					enkeltValgStreng_1 = "nada";
+				streng_1 = (String) ((JRFillParameter) parametersMap.get("streng_1")).getValue();
+				if (streng_1 == null) {
+					streng_1 = "nada";
 				}
-				rconn.voidEval("enkeltValgStreng_1=" + "'" + enkeltValgStreng_1 + "'");
+				rconn.voidEval("streng_1=" + "'" + streng_1 + "'");
 			} catch (Exception e) {
-				log.debug("Parameter enkeltValgStreng_1 is not defined: " + e.getMessage());
+				log.debug("Parameter streng_1 is not defined: " + e.getMessage());
 			}
 
-			Integer heltall_1;
+			String streng_2;
 			try {
-				heltall_1 = (Integer) ((JRFillParameter) parametersMap.get("heltall_1")).getValue();
-				if (heltall_1 == null) {
-					heltall_1 = 0;
+				log.debug("Getting parameter values");
+				streng_2 = (String) ((JRFillParameter) parametersMap.get("streng_2")).getValue();
+				if (streng_2 == null) {
+					streng_2 = "nada";
 				}
-				rconn.voidEval("heltall_1=" + heltall_1.toString());
+				rconn.voidEval("streng_2=" + "'" + streng_2 + "'");
 			} catch (Exception e) {
-				log.debug("Parameter heltall_1 is not defined: " + e.getMessage());
+				log.debug("Parameter streng_2 is not defined: " + e.getMessage());
 			}
 
+			String streng_3;
+			try {
+				log.debug("Getting parameter values");
+				streng_3 = (String) ((JRFillParameter) parametersMap.get("streng_3")).getValue();
+				if (streng_3 == null) {
+					streng_3 = "nada";
+				}
+				rconn.voidEval("streng_3=" + "'" + streng_3 + "'");
+			} catch (Exception e) {
+				log.debug("Parameter streng_3 is not defined: " + e.getMessage());
+			}
+
+			String streng_4;
+			try {
+				log.debug("Getting parameter values");
+				streng_4 = (String) ((JRFillParameter) parametersMap.get("streng_4")).getValue();
+				if (streng_4 == null) {
+					streng_4 = "nada";
+				}
+				rconn.voidEval("streng_4=" + "'" + streng_4 + "'");
+			} catch (Exception e) {
+				log.debug("Parameter streng_4 is not defined: " + e.getMessage());
+			}
+
+			String streng_5;
+			try {
+				log.debug("Getting parameter values");
+				streng_5 = (String) ((JRFillParameter) parametersMap.get("streng_5")).getValue();
+				if (streng_5 == null) {
+					streng_5 = "nada";
+				}
+				rconn.voidEval("streng_5=" + "'" + streng_5 + "'");
+			} catch (Exception e) {
+				log.debug("Parameter streng_5 is not defined: " + e.getMessage());
+			}
+			
 			Double desimaltall_1;
 			try {
 				desimaltall_1 = (Double) ((JRFillParameter) parametersMap.get("desimaltall_1")).getValue();
@@ -260,6 +426,50 @@ public class CommonReport extends JRDefaultScriptlet {
 				log.debug("Parameter desimaltall_1 is not defined: " + e.getMessage());
 			}
 
+			Double desimaltall_2;
+			try {
+				desimaltall_2 = (Double) ((JRFillParameter) parametersMap.get("desimaltall_2")).getValue();
+				if (desimaltall_2 == null) {
+					desimaltall_2 = 0.0;
+				}
+				rconn.voidEval("desimaltall_2=" + desimaltall_2.toString());
+			} catch (Exception e) {
+				log.debug("Parameter desimaltall_2 is not defined: " + e.getMessage());
+			}
+
+			Double desimaltall_3;
+			try {
+				desimaltall_3 = (Double) ((JRFillParameter) parametersMap.get("desimaltall_3")).getValue();
+				if (desimaltall_3 == null) {
+					desimaltall_3 = 0.0;
+				}
+				rconn.voidEval("desimaltall_3=" + desimaltall_3.toString());
+			} catch (Exception e) {
+				log.debug("Parameter desimaltall_3 is not defined: " + e.getMessage());
+			}
+
+			Double desimaltall_4;
+			try {
+				desimaltall_4 = (Double) ((JRFillParameter) parametersMap.get("desimaltall_4")).getValue();
+				if (desimaltall_4 == null) {
+					desimaltall_4 = 0.0;
+				}
+				rconn.voidEval("desimaltall_4=" + desimaltall_4.toString());
+			} catch (Exception e) {
+				log.debug("Parameter desimaltall_4 is not defined: " + e.getMessage());
+			}
+
+			Double desimaltall_5;
+			try {
+				desimaltall_5 = (Double) ((JRFillParameter) parametersMap.get("desimaltall_5")).getValue();
+				if (desimaltall_5 == null) {
+					desimaltall_5 = 0.0;
+				}
+				rconn.voidEval("desimaltall_5=" + desimaltall_5.toString());
+			} catch (Exception e) {
+				log.debug("Parameter desimaltall_5 is not defined: " + e.getMessage());
+			}
+			
 			Date dato_1;
 			try {
 				dato_1 = (Date) ((JRFillParameter) parametersMap.get("dato_1")).getValue();
@@ -270,6 +480,54 @@ public class CommonReport extends JRDefaultScriptlet {
 				rconn.voidEval("dato_1=" + "'" + datoString + "'");
 			} catch (Exception e) {
 				log.debug("Parameter dato_1 is not defined: " + e.getMessage());
+			}
+
+			Date dato_2;
+			try {
+				dato_2 = (Date) ((JRFillParameter) parametersMap.get("dato_2")).getValue();
+				if (dato_2 == null) {
+					dato_2 = new Date();
+				}
+				StringBuilder datoString = new StringBuilder(rFormat.format(dato_2));
+				rconn.voidEval("dato_2=" + "'" + datoString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter dato_2 is not defined: " + e.getMessage());
+			}
+
+			Date dato_3;
+			try {
+				dato_3 = (Date) ((JRFillParameter) parametersMap.get("dato_3")).getValue();
+				if (dato_3 == null) {
+					dato_3 = new Date();
+				}
+				StringBuilder datoString = new StringBuilder(rFormat.format(dato_3));
+				rconn.voidEval("dato_3=" + "'" + datoString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter dato_3 is not defined: " + e.getMessage());
+			}
+
+			Date dato_4;
+			try {
+				dato_4 = (Date) ((JRFillParameter) parametersMap.get("dato_4")).getValue();
+				if (dato_4 == null) {
+					dato_4 = new Date();
+				}
+				StringBuilder datoString = new StringBuilder(rFormat.format(dato_4));
+				rconn.voidEval("dato_4=" + "'" + datoString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter dato_4 is not defined: " + e.getMessage());
+			}
+
+			Date dato_5;
+			try {
+				dato_5 = (Date) ((JRFillParameter) parametersMap.get("dato_5")).getValue();
+				if (dato_5 == null) {
+					dato_5 = new Date();
+				}
+				StringBuilder datoString = new StringBuilder(rFormat.format(dato_5));
+				rconn.voidEval("dato_5=" + "'" + datoString + "'");
+			} catch (Exception e) {
+				log.debug("Parameter dato_5 is not defined: " + e.getMessage());
 			}
 			// END generic, common report user controls
 			  
